8ec9635aa9303b7ce2d590b94870b0a2ae2f37c1
krzemyk <113865837+krzemyk84@users.noreply.github.com>
2022-10-10 20:24:07 +0000

    Working ver 2

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const paper = 2.5;
 const start_cookies_program = () => {
   var selling_price_per_cookie = prompt ("Please enter price of one cookie: ");
   //alert (selling_price_per_cookie);
-  
   var cost_per_batch = flour + butter + eggs + sugar + flavour + fuel + paper;
   alert ("Cost per batch is: " + cost_per_batch.toFixed(2));
   var cost_per_cookie = cost_per_batch / 50;
@@ -17,8 +16,26 @@ const start_cookies_program = () => {
   var profit_per_cookie = selling_price_per_cookie - cost_per_cookie;
   alert ("Profit per cookie is: " + profit_per_cookie.toFixed(2));
   var profit_per_batch = profit_per_cookie * 50;
-  alert ("Profit per batch is: " + profit_per_batch.toFixed(2));  
+  alert ("Profit per batch is: " + profit_per_batch.toFixed(2));
+  const want_restart = prompt ("Do you want to try it again? (y/n) ");
+  if (check_yes_or_no(want_restart)) {
+    restart_app();
+  }
+  else {
+    prompt ("Bye!");
+  }
 }
+const check_yes_or_no = (want_restart) => {
+  if (want_restart == "y") {
+    return true;
+  }
+  else return false;
+}
+
+
+const restart_app = () => {
+  start_cookies_program ();
+  }
 
 start_cookies_program (); 
 
